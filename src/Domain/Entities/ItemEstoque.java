efec6aa3a5d4a90ae88548f1eb35962c254e393a
Mickael Araujo <mickaelaraujo.contato@gmail.com>
2023-11-19 15:10:38 -0300

    wip: Create base stock entity

--- src/Domain/Entities/ItemEstoque.java
+++ src/Domain/Entities/ItemEstoque.java
@@ -3,11 +3,13 @@ package Domain.Entities;
 public class ItemEstoque extends Entidade {
     private ItemBiblioteca item;
     private int quantidade;
+    private double preco;
 
-    public ItemEstoque(ItemBiblioteca item, int quantidade) {
+    public ItemEstoque(ItemBiblioteca item, int quantidade, double preco) {
         super();
         this.setItem(item);
         this.setQuantidade(quantidade);
+        this.setPreco(preco);
     }
 
     public ItemBiblioteca getItem() {
@@ -26,11 +28,20 @@ public class ItemEstoque extends Entidade {
         this.quantidade = quantidade;
     }
 
+    public double getPreco() {
+        return this.preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
     @Override
     public String toString() {
         return "ItemEstoque{" +
                 "item=" + this.getItem().toString() +
                 ", quantidade=" + this.getQuantidade() +
+                ", preco=" + this.getPreco() +
                 '}';
     }
 }
